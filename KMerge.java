9d20a84134acc7d1d070394508ff764678013c39
dielhennr <rdielhenn@dons.usfca.edu>
2018-02-22 15:10:04 -0800

    revisions

--- KMerge.java
+++ KMerge.java
@@ -1,88 +1,114 @@
+/**
+ * 
+ */
+
 public class KMerge {
-    int[] numArr;
-    int[] helperArr;
-    /*
-    public static void main(String[] args) {
-    	int[] arr = {4,1, 2,5, 2,7, 3,8, 6,7,8,10,9};
-    	KMerge merger = new KMerge();
-    	merger.sort(arr, 5);
-    }
-    */
+	int[] numArr;
+	int[] helperArr;
 
-    public void sort(int[] numArr, int KPartitions) {
-    	this.numArr = numArr;
-    	this.helperArr = new int[this.numArr.length];
-    	for (int i = 0; i < numArr.length; i++) {
-    		this.helperArr[i] = this.numArr[i];
-    	}
-        kWayMerge(numArr.length/KPartitions, 0, this.numArr.length, KPartitions);
-    }
-    public static String toString(int[] arr){
-    	String s = "";
-    	int i;
-    	for (i = 0; i < arr.length; i++) {
-    		s += arr[i] + "\n";
-    	}
-    	return s;
-    }
-    
-    public void kWayMerge(int sizePartitions, int startPart, int endPart, int KPartitions) {
-    	int sizeArr = endPart - startPart;
-    	int sizeLastPartition = sizeArr%KPartitions;
-       	int length = sizePartitions;
-       	int totalSize = sizeArr;
+	public void sort(int[] numArr, int KPartitions) {
+		this.numArr = numArr;
+		this.helperArr = new int[this.numArr.length];
+		for (int i = 0; i < numArr.length; i++) {
+			this.helperArr[i] = this.numArr[i];
+		}
+		kWayMerge(this.numArr.length/KPartitions, 0, this.numArr.length, KPartitions);
+	}
+	public static String toString(int[] arr){
+		String s = "";
+		int i;
+		for (i = 0; i < arr.length; i++) {
+			s += arr[i] + "\n";
+		}
+		return s;
+	}
 
-       	for (length = sizePartitions; length <= totalSize; length*=2){
-       		int i = 0;
+	/** 
+	 * Given a partion in an array KWayMerge recursivley sorts the given partition.
+	 * The input partition is split into K new partitions, and each of those partitions are passed
+	 * back into KWayMerge. Once the partition size is equal to one, it begins a two way merge from bottom up.
+	 * Suppose there are k sorted partitions of size n/k for the first k-1 partitions and size n%k for the last partition.
+	 * the first two will be merged, then the next two, then the next two.... 
+	 * Now there are k/2 partitions if k is even and k/2 + 1 if k is odd.
+	 * This process is reapeated until there are 2 sorted partitions, they will merge and then we are done.
+	 * If k is odd, the last partition will be merged last with the rest of the elements.
+	 * @param startPart: start index of the partition
+	 *		  endPart: end index of the partition
+	 *		  KPartitions: number of partitions to split given partition into
+	 * @return void
+	 */
 
-       		while(i + length < totalSize) {
-       			
-       			int high;
-       			if(i+2*length < totalSize){
-       				high = i+2*length;
-       			}else{
-       				high = totalSize;
-       			}
-       			if (sizePartitions < KPartitions) {
-       				KPartitions = sizePartitions;
-       			}
-       			if (sizePartitions > 1){
-       				kWayMerge(Math.max(sizePartitions/KPartitions, 1), i, high, KPartitions);
-       			}
-       			twoWayMerge(i, high, i + length);
-       			i += 2*length;
-       		}
-       	}
+	public void kWayMerge(int sizePartitions, int startPart, int endPart, int KPartitions) {
 
-    }
-    public void twoWayMerge(int low, int high, int mid) {
-    	int i = low;
-    	int j = low;
-    	int k = mid;
-    	
- 		while (i < mid && k < high) {
+		//length of partition (n)
+		int totalSize = endPart - startPart;
+		
+		for (int length = sizePartitions; length <= totalSize; length*=2){
+			int i = 0;
 
- 			if (this.helperArr[i] <= this.helperArr[k]) {	
- 				this.numArr[j++] = this.helperArr[i++];
+			//either call kwaymerge on the partition, or two way merge with the partitions neighbor.
+			while(i + length < totalSize) {
 
- 			}
- 			else if (this.helperArr[i] > this.helperArr[k]) {
- 				this.numArr[j++]= this.helperArr[k++];
- 			}
- 		}
+				int high;
+				if(i+2*length < totalSize){
+					high = i+2*length;
+				}else{
+					high = totalSize;
+				}
+				/**
+				 * If the size of a given partition is less than k, we set the new k to the size of that partition
+				 * for the recursive call.
+				 */
+				if (sizePartitions < KPartitions) {
+					KPartitions = sizePartitions;
+				}
 
- 		while (i < mid) {
- 			this.numArr[j++] = this.helperArr[i++];
- 		}
+				if (sizePartitions > 1){
+					kWayMerge(Math.max(sizePartitions/KPartitions,1), i, high, KPartitions);
+				}
+				/**
+				 * If the size of a partition is 1, we no longer need to recurse, begin two way merge with
+				 * the partitions neighbor.
+				*/
 
- 		while (k < high) {
- 			this.numArr[j++] = this.helperArr[k++];
- 		}
+				/*
 
- 		while(low < high) {
- 			this.helperArr[low] = this.numArr[low++];
+				*/
+				twoWayMerge(i, i + length, high);
+				i += 2*length;
+			}
+		}
 
- 		}
+	}
+	
+	public void twoWayMerge(int low, int mid, int high) {
+		int i = low;
+		int j = low;
+		int k = mid;
 
-    }
+		while (i < mid && k < high) {
+
+			if (this.helperArr[i] <= this.helperArr[k]) {	
+				this.numArr[j++] = this.helperArr[i++];
+
+			}
+			else if (this.helperArr[i] > this.helperArr[k]) {
+				this.numArr[j++]= this.helperArr[k++];
+			}
+		}
+
+		while (i < mid) {
+			this.numArr[j++] = this.helperArr[i++];
+		}
+
+		while (k < high) {
+			this.numArr[j++] = this.helperArr[k++];
+		}
+
+		while(low < high) {
+			this.helperArr[low] = this.numArr[low++];
+
+		}
+
+	}
 }
--- KMergeDriver.java
+++ KMergeDriver.java
@@ -9,42 +9,47 @@ public class KMergeDriver {
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.out.println("Usage: java KMergeDriver <file.txt>");
-            System.exit(0);
+			System.exit(0);
 		}
-		File file = new File(args[0]);
-		try (Scanner scan = new Scanner(file)) {
 
+		File file = new File(args[0]);
+		if (!file.exists()) {
+			System.out.println("File Not Found");
+			System.exit(0);
+		}
+
+		try (Scanner scan = new Scanner(file)) {
 			scan.nextLine();
 			int count = 0;
 			while (scan.hasNextLine()) {
 				count++;
 				scan.nextLine();
 			}
-            scan.close();
+			scan.close();
 			Scanner scan1 = new Scanner(file);
 
 			int k = scan1.nextInt();
-            int[] numbers = new int[count];
-            for (int i = 0; i < count; i++) {
-                numbers[i] = scan1.nextInt();
-                System.out.println(numbers[i]);
-            }
-            scan1.close();
+			int[] numbers = new int[count];
+			String strNum;
+			for (int i = 0; i < count; i++) {
+				numbers[i] = scan1.nextInt();
+			}
+			scan1.close();
 
-            KMerge merger = new KMerge();
-            merger.sort(numbers, k);
-            try{
-	            File outFile = new File(args[0] + "_sorted");
-	            outFile.createNewFile();
-	      		FileWriter writer = new FileWriter(outFile); 
-	      		writer.write(merger.toString(numbers)); 
-	      		writer.flush();
-	      		writer.close();
-	      	}catch (IOException ioe) {
-	      		System.out.println("Error when writing output to file");
-	      	}
+			KMerge merger = new KMerge();
+			merger.sort(numbers, k);
+			try{
+				File outFile = new File(args[0] + "_sorted");
+				outFile.createNewFile();
+				FileWriter writer = new FileWriter(outFile); 
+				writer.write(merger.toString(numbers)); 
+				writer.flush();
+				writer.close();
+			}catch (IOException ioe) {
+				System.out.println("Error when writing output to file");
+			}
 
-            
+
 
 		} catch (FileNotFoundException fnf) {
 
